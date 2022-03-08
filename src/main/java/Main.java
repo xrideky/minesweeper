7f89ea50cdc104884bb551143233b50ab35cd094
xrideky <xrideky@stuba.sk>
2022-03-08 15:23:28 +0100

    board and game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
-package minesweeper.utility;
+import game.Game;
 
 public class Main {
     public static void main(String[] args){
+        Game g=new Game();
+        g.play();
 
     }
 }
---
+++ src/main/java/game/Game.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Game {
+
+    private Board board;
+
+     public Game() {
+        this.board=new Board();
+    }
+
+    public void play(){
+        System.out.println(this.board.draw());
+    }
+}
